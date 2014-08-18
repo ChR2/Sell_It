c5c504a1039956dc6de87229d79cec9b9d4b55c2
Wendy Sanchez-Vaynshteyn <WendyGSV@gmail.com>
2014-08-17 22:07:27 -0400

    fixed rollback issue for itemrequest#create

--- app/controllers/item_requests_controller.rb
+++ app/controllers/item_requests_controller.rb
@@ -7,9 +7,10 @@ class ItemRequestsController < ApplicationController
 
   def create
     @items = params[:item].map {|item_id| Item.find(item_id)}
-    @item_request = ItemRequest.new(items: @items, message: params[:message], email: params[:email])
+    @item_request = ItemRequest.new(items: @items, message: params[:item_request][:message], email: params[:item_request][:email])
     if @item_request.save
-      #do Something
+      flash[:notice] = "Your message was created successfully!"
+      redirect_to root_path
     else
       flash[:notice] = @item_request.errors.full_messages
       render :new
