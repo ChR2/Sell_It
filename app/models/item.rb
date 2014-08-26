a8686515b820d5b8e28f04825817acf167df4b24
Wendy Sanchez-Vaynshteyn <WendyGSV@gmail.com>
2014-08-26 00:38:18 -0400

    revised app, modified controllers and queries

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,6 @@ class Item < ActiveRecord::Base
   has_many :item_request_items
   has_many :item_requests, through: :item_request_items
 
-  validates_presence_of :title, :description, :price
-  validates :price, numericality: {greater_than: 0}
-
-
-
+  validates :title, :description, presence: true
+  validates :price, presence: true, numericality: {greater_than: 0}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'items#index'
-  resources :items, only: [:index, :show]
+  resources :items, only: [:show]
   resources :item_requests, only: [:new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
